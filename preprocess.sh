91659c419e8c35c27237958c03011b43a71096d6
Taras Lishchenko <jackersson@meta.ua>
2020-07-01 19:50:06 +0300

    added arguments for preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,18 @@
 TEMP_FILE=all_files.txt
 
-IN_FOLDER=/home/taras/coder/projects/sdf-research/data/planes_sample
-OUT_FOLDER=/home/taras/coder/projects/sdf-research/sdf/parallel
+IN_FOLDER=$1
+if [ -z "$1" ]
+  then
+    echo "Error: must specifiy folder with mesh (*.obj) video"
+    exit -1
+fi
+
+OUT_FOLDER=$2
+if [ -z "$2" ]
+  then
+    echo "Error: must specifiy out folder for (*.npz)"
+    exit -1
+fi
 
 find $IN_FOLDER -type f -name "*.obj" > $TEMP_FILE
 while read filepath; do
@@ -9,5 +20,4 @@ while read filepath; do
     filename="${filename%.*}"
     ./DeepSDF/bin/PreprocessMesh -m $filepath -o "$OUT_FOLDER/$filename.npz" -t &
     [ $( jobs | wc -l ) -ge $( nproc ) ] && wait & echo "Done $filepath"
-    # echo "Done $filepath"
 done < $TEMP_FILE
